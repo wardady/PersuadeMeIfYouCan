fe53b700a35d7d9113fc2d7cc64ecbc7be237913
Hermann <yavorskyi@ucu.edu.ua>
2019-06-01 23:35:36 +0300

    Registration complete

--- bot.py
+++ bot.py
@@ -9,12 +9,14 @@ woman_emoji = emojize(":woman:")
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
+    print(message)
     # TODO Check if user is in DB if not then add to DB and continue this branch, otherwise *new branch to be written*
     chat_id = message.chat.id
     markup = telebot.types.ReplyKeyboardMarkup(row_width=2)
     markup.add(telebot.types.KeyboardButton(man_emoji), telebot.types.KeyboardButton(woman_emoji))
-    bot.send_message(chat_id, "Hi! You seem to be a new user. Let`s fill some information. Choose your sex:",
-                     reply_markup=markup)
+    new_msg = bot.send_message(chat_id, "Hi! You seem to be a new user. Let`s fill some information. Choose your sex:",
+                               reply_markup=markup)
+    bot.register_next_step_handler(new_msg, process_sex)
 
 
 @bot.message_handler(commands=['help'])
@@ -23,13 +25,32 @@ def send_welcome(message):
     bot.send_message(chat_id, "***HELP INFORMATION TO BE PROVIDED HERE***")
 
 
-@bot.message_handler(func=lambda x: x.text in [man_emoji, woman_emoji])
 def process_sex(x):
-    # TODO add sex to user in DB
-    if x.text == man_emoji:
-        print('man')
+    chat_id = x.chat.id
+    markup = telebot.types.ReplyKeyboardMarkup()
+    if x.text in (man_emoji, woman_emoji):
+        # TODO add sex to user in DB
+        markup = telebot.types.ReplyKeyboardRemove(selective=False)
+        new_msg = bot.send_message(chat_id,
+                                   "Thanks, now tell me your age (please input only number):",
+                                   reply_markup=markup)
+        bot.register_next_step_handler(new_msg, process_age)
+        print('Sex_registered')
     else:
-        print('woman')
+        new_msg = bot.send_message(chat_id, "Ooops... Wrong input, please, choose from the options below.")
+        bot.register_next_step_handler(new_msg, process_sex)
+
+
+def process_age(message):
+    chat_id = message.chat.id
+    if str.isalnum(message.text):
+        # TODO add age to DB
+        age = int(message.text)
+        print(age)
+        pass
+    else:
+        new_msg = bot.send_message(chat_id, "Please, input only number:")
+        bot.register_next_step_handler(new_msg, process_age)
 
 
 @bot.message_handler(func=lambda x: True)
