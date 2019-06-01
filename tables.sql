4c6fab47af728e14494eb01eb763a91fa2f0d992
olesyat <34340614+olesyat@users.noreply.github.com>
2019-06-01 23:15:37 +0300

    Create tables sql

---
+++ tables.sql
@@ -0,0 +1,8 @@
+CREATE TABLE user(id int PRIMARY KEY AUTO_INCREMENT, id_tl int, sex BOOLEAN not null, age int not null)
+
+CREATE TABLE question(id int PRIMARY KEY AUTO_INCREMENT, name varchar(255), category varchar(255))
+
+CREATE TABLE response(id int PRIMARY KEY AUTO_INCREMENT,
+  question_id int, FOREIGN KEY (question_id) REFERENCES question(id) not null,
+  yes_id int, FOREIGN KEY (yes_id) REFERENCES user(id),
+  no_id int, FOREIGN KEY (yes_id) REFERENCES user(id))
